04280ce8a41549d4d0a891acf2d20b5cef114af7
Parkyeseul <from.yeseul@gmail.com>
2022-07-08 17:46:56 +0900

    로그인, 프로젝트 의로목록

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,177 @@
 
 /* 레이어팝업(여러개ok) - overflow, 외부영역선택시 close 추가해보기 */
-let openBtn = document.querySelectorAll(".layerOpen");
-let closeBtn = document.querySelectorAll(".layerClose");
-let layerID;
+function layerPop(){
+    let openBtn = document.querySelectorAll(".layerOpen");
+    let closeBtn = document.querySelectorAll(".layerClose");
+    let layerID;
 
-for(let i=0; i<openBtn.length; i++){
-    openBtn[i].addEventListener("click", function(){
-        layerID = this.getAttribute('href');
-        document.querySelector(layerID).classList.add('is-active');
-    });
+    for(let i=0; i<openBtn.length; i++){
+        openBtn[i].addEventListener("click", function(){
+            layerID = this.getAttribute('href');
+            document.querySelector(layerID).classList.add('is-active');
+        });
+    }
+    for(let j=0; j<openBtn.length; j++){
+        closeBtn[j].addEventListener('click', function(){
+            this.parentNode.parentNode.parentNode.classList.remove('is-active');
+        });
+    }
 }
-for(let j=0; j<openBtn.length; j++){
-    closeBtn[j].addEventListener('click', function(){
-        this.parentNode.parentNode.parentNode.classList.remove('is-active');
-    });
-}
+layerPop();
 
 
 /* 탭메뉴(여러개ok) */    
-let tabBtn = document.querySelectorAll('.tabBtnWrap a');
-let tabCont = document.querySelectorAll('.tabContWrap > div');
+function tabMenu(){
+    let tabBtn = document.querySelectorAll('.tabBtnWrap a');
+    let tabCont = document.querySelectorAll('.tabContWrap > div');
 
-for(let i=0; i < tabBtn.length; i++){
-    tabBtn[i].addEventListener('click',function(e){
+    for(let i=0; i < tabBtn.length; i++){
+        tabBtn[i].addEventListener('click',function(e){
 
-        let tab_id = this.closest('.tabWrapper').id;
+            let tab_id = this.closest('.tabWrapper').id;
 
-        e.preventDefault();
-        let tabHref = e.target.getAttribute('href');    //e의 역할?????
-        let tabTarget = tabHref.replace('#','');
+            e.preventDefault();
+            let tabHref = e.target.getAttribute('href');    //e의 역할?????
+            let tabTarget = tabHref.replace('#','');
 
-        // for(let x=0; x < tabCont.length; x++){
-        //     if (tabCont[x].closest('.tabWrapper').id == tab_id){
-        //         tabCont[x].style.display='none';
-        //     }
-        // }
-        //document.getElementById(tabTarget).style.display="block";
+            // for(let x=0; x < tabCont.length; x++){
+            //     if (tabCont[x].closest('.tabWrapper').id == tab_id){
+            //         tabCont[x].style.display='none';
+            //     }
+            // }
+            //document.getElementById(tabTarget).style.display="block";
 
-        for(let j=0; j < tabBtn.length; j++){
-            if (tabBtn[j].closest('.tabWrapper').id == tab_id){
-                tabBtn[j].classList.remove('is-active');
-                e.target.classList.add('is-active');
-                tabCont[j].style.display='none';   //왜 remove is-active는 안되지???? ,  왜 i로 통일은 안되지????
+            for(let j=0; j < tabBtn.length; j++){
+                if (tabBtn[j].closest('.tabWrapper').id == tab_id){
+                    tabBtn[j].classList.remove('is-active');
+                    e.target.classList.add('is-active');
+                    tabCont[j].style.display='none';   //???????? 왜 remove is-active는 안되지???? ,  ???????? 왜 i로 통일은 안되지????
+                }
             }
-        }
-        document.getElementById(tabTarget).style.display="block";
-    });
-}        
+            document.getElementById(tabTarget).style.display="block";
+        });
+    }        
+}
+tabMenu();
 
 
 /* GNB - nav */
-let layoutWrapper = document.querySelector('.layoutWrapper');
-let navCtrlBtn = document.querySelector('.navCtrlBtn');
-let navWrapper = document.querySelector('.navWrapper');
+// function gnbNav(){
+//     let layoutWrapper = document.querySelector('.layoutWrapper');
+//     let navCtrlBtn = document.querySelector('.navCtrlBtn');
+//     let navWrapper = document.querySelector('.navWrapper');
 
-navCtrlBtn.addEventListener('click', function(){
-    layoutWrapper.classList.toggle('is-active');
-});
+//     navCtrlBtn.addEventListener('click', function(){
+//         layoutWrapper.classList.toggle('is-active');
+//     });
 
-navWrapper.addEventListener('mouseenter', function(){
-    layoutWrapper.classList.add('is-hover');
-});
-navWrapper.addEventListener('mouseleave', function(){
-    layoutWrapper.classList.remove('is-hover');
-});
+//     navWrapper.addEventListener('mouseenter', function(){
+//         layoutWrapper.classList.add('is-hover');
+//     });
+//     navWrapper.addEventListener('mouseleave', function(){
+//         layoutWrapper.classList.remove('is-hover');
+//     });
+// }
+// gnbNav();
 
 
+/* MYPROJECT 레이아웃 */
+// function contLayout(){
+//     let contLayout = document.querySelector('.contLayout');
+//     let myProjDetail = document.querySelector('.myProjDetail');
 
-/* CONTENT 레이아웃 - myProject */
-let contLayout = document.querySelector('.contLayout');
-let myProjDetail = document.querySelector('.myProjDetail');
-
-myProjDetail.addEventListener('click', function(){
-    contLayout.classList.add('is-active');
-});
+//     myProjDetail.addEventListener('click', function(){
+//         contLayout.classList.add('is-active');
+//     });
+// }
+// contLayout();
 
 
 /* 우측패널 */
-let rPanelList = document.querySelectorAll('.rPanel > ul > li');
-let rPanelLayer = document.querySelector('.rPanelLayer');
+function rightPanel(){
+    let rPanelList = document.querySelectorAll('.rPanel > ul > li');
+    let rPanelLayer = document.querySelector('.rPanelLayer');
 
-for(let i=0; i<rPanelList.length; i++){
-    rPanelList[i].addEventListener('click', function(){
-        rPanelLayer.classList.toggle('is-active');
+    for(let i=0; i<rPanelList.length; i++){
+        rPanelList[i].addEventListener('click', function(){
+            rPanelLayer.classList.toggle('is-active');
 
-        let windowWidth = window.innerWidth;
-        if(windowWidth <= 1000){
-            rPanelLayer.classList.toggle('layerType');
-        }
-    });
+            let windowWidth = window.innerWidth;
+            if(windowWidth <= 1000){
+                rPanelLayer.classList.toggle('layerType');
+            }
+        });
+    }
 }
+rightPanel();
 
 
 /* 패스워드 보이기 */
-let showPw = document.querySelector('.showPw');
-    
-showPw.addEventListener('click', function(){
-    let inputPw = this.nextElementSibling;
-    let inputPwType = inputPw.getAttribute("type");
-    
-    if(inputPwType === "password"){
-        showPw.classList.add('is-active');
-        inputPw.setAttribute("type", "text");
-    }else{
-        showPw.classList.remove('is-active');
-        inputPw.setAttribute("type", "password");
-    }
+function showPw(){
+    let showPw = document.querySelector('.showPw');
+        
+    showPw.addEventListener('click', function(){
+        let inputPw = this.nextElementSibling;
+        let inputPwType = inputPw.getAttribute("type");
+        
+        if(inputPwType === "password"){
+            showPw.classList.add('is-active');
+            inputPw.setAttribute("type", "text");
+        }else{
+            showPw.classList.remove('is-active');
+            inputPw.setAttribute("type", "password");
+        }
+    });
+}
+showPw();
+
+
+
+/* 전체선택, 해제 
+$(".chkWrap").on("click", "#chkAll", function () {
+    $(this).parents(".chkWrap").find('input').prop("checked", $(this).is(":checked"));
 });
+
+// 체크박스 개별 선택
+$(".chkWrap").on("click", ".chkEach", function() {
+    var is_checked = true;
+
+    $(".chkWrap .chkEach").each(function(){
+        is_checked = is_checked && $(this).is(":checked");
+    });
+
+    $("#chkAll").prop("checked", is_checked);
+});*/
+
+/* 체크박스 전체선택 */     //??????????????? 여러개 구별하도록 수정하기
+function chkAll(){
+    let chkAll = document.querySelector('.chkAll');
+    let chkEach = document.querySelectorAll('.chkEach');
+
+        for(let i=0; i<chkEach.length; i++){  
+            //전체 선택, 해제
+            chkAll.addEventListener('click', function(){
+                if(chkAll.checked == true){
+                    chkEach[i].checked = true;
+                }else{
+                    chkEach[i].checked = false;
+                }            
+            });
+
+            //개별선택시 전체 선택, 해제
+            chkEach[i].addEventListener('click', function(){        
+                let chked = document.querySelectorAll('.chkEach:checked');
+                if(chked.length == chkEach.length){
+                    chkAll.checked = true;
+                }else{
+                    chkAll.checked = false;
+                }
+            });
+        }
+}
+chkAll();
+
+
+//??????????????????
+//html에 없는 script일 경우 Uncaught TypeError : addEventListner 가
+//뜨면서 이하에 있는 script가 적용이 안됨
